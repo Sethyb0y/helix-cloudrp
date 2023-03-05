908277361be4e6f474db1b2701b45b1810d86705
Sethyb0y <100891438+Sethyb0y@users.noreply.github.com>
2023-03-05 14:11:46 -0600

    0.2
    Added Conscript Faction

--- schema/items/sh_cid.lua
+++ schema/items/sh_cid.lua
@@ -1,7 +1,7 @@
 
-ITEM.name = "Citizen ID"
+ITEM.name = "Indentification Card"
 ITEM.model = Model("models/gibs/metal_gib4.mdl")
-ITEM.description = "A citizen identification card with ID #%s, assigned to %s."
+ITEM.description = "An identification card with ID #%s, assigned to %s."
 
 function ITEM:GetDescription()
 	return string.format(self.description, self:GetData("id", "00000"), self:GetData("name", "nobody"))
