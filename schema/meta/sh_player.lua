4bd8d9fc463a7fbbed86240cb37915ebc8b837e4
Sethyb0y <100891438+Sethyb0y@users.noreply.github.com>
2023-03-11 16:56:17 -0600

    ismale()

--- schema/meta/sh_player.lua
+++ schema/meta/sh_player.lua
@@ -1,15 +1,15 @@
 local playerMeta = FindMetaTable("Player")
 
-function meta:IsMale()
-	local model = self:GetModel():lower()
-
-	return (model:find("male") or model:find("alyx") or model:find("mossman")) != nil or
-		ix.anim.GetModelClass(model) == "citizen_male"
-end
-
 function playerMeta:IsCombine()
 	local faction = self:Team()
 	return faction == FACTION_MPF or faction == FACTION_OTA
+end
+
+function playerMeta:IsMale()
+	local model = self:GetModel():lower()
+
+	return (model:find("male") or model:find("art") or model:find("erdim") or model:find("eric") or model:find("joe") or model:find("van") or model:find("mike") or model:find("ted") or model:find("vance")) != nil or
+		ix.anim.GetModelClass(model) == "citizen_male"
 end
 
 function playerMeta:IsDispatch()
